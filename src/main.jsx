f13f36b2cdb26e2c43ba6e7e62933ef305ddcb26
Rahul Lokhande <rlokhande06244@gmail.com>
2024-09-03 11:56:11 +0530

    Main File

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import "./css/style.css";
+import CourseContext from './CourseContext.jsx';
+ReactDOM.createRoot(document.getElementById('root')).render(
+<CourseContext>
+<App />
+</CourseContext>
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
 )

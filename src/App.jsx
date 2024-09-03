4761cbc66ac2c90104cf7ed58827772846eaadfe
Rahul Lokhande <rlokhande06244@gmail.com>
2024-09-03 11:53:23 +0530

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,57 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Layout from "./Layout";
-import Home from "./pages/Home";
-import Insert from "./pages/Insert";
-import Display from "./pages/Display";
-import DoctorProfile from "./pages/DoctoProfile";
-import Search from "./pages/Search";
-import DoctorGouravPatel from "./pages/DoctorId01";
-import DoctorArunSharma from "./pages/DoctorId02";
-import DoctorRituBaghel from "./pages/DoctorId03";
-import DoctorVarshaGupta from "./pages/DoctorId04";
+// import { BrowserRouter,Routes,Route } from "react-router-dom";
+// import Layout from "./Layout";
+// import Home from "./pages/Home";
+// import Insert from "./pages/Insert";
+// import Display from "./pages/Display";
+// import DoctorProfile from "./pages/DoctoProfile";
+// import Search from "./pages/Search";
+// import DoctorGouravPatel from "./pages/DoctorId01";
+// import DoctorArunSharma from "./pages/DoctorId02";
+// import DoctorRituBaghel from "./pages/DoctorId03";
+// import DoctorVarshaGupta from "./pages/DoctorId04";
+
+// const App=()=>{
+//   return(
+//     <>
+//        <BrowserRouter>
+//                <Routes>
+//                       <Route path="/" element={<Layout/>}>
+//                              <Route index element={<Home/>}/>
+//                              <Route path="home" element={<Home/>}/>
+//                              <Route path="insert" element={<Insert/>}/>
+//                              <Route path="display" element={<Display/>}/>
+//                              <Route path="doctorprofile" element={<DoctorProfile/>}/>
+//                              <Route path="search" element={<Search/>}/>
+//                              <Route path="doctor_gourav" element={<DoctorGouravPatel/>}/>
+//                              <Route path="doctor_Arun" element={<DoctorArunSharma/>}/>
+//                              <Route path="doctor_ritu" element={<DoctorRituBaghel/>}/>
+//                              <Route path="doctor_varsha" element={<DoctorVarshaGupta/>}/>
+
+//                       </Route>
+//                </Routes>
+//        </BrowserRouter>
+//     </>
+//   )
+// }
+// export default App;
+
+
+
+
+
+import Bhopal from "./Bhopal"
+import { myContext } from "./CourseContext"
+import { useContext } from "react"
 
 const App=()=>{
+
+  const {course,setCourse}=useContext(myContext)
   return(
     <>
-       <BrowserRouter>
-               <Routes>
-                      <Route path="/" element={<Layout/>}>
-                             <Route index element={<Home/>}/>
-                             <Route path="home" element={<Home/>}/>
-                             <Route path="insert" element={<Insert/>}/>
-                             <Route path="display" element={<Display/>}/>
-                             <Route path="doctorprofile" element={<DoctorProfile/>}/>
-                             <Route path="search" element={<Search/>}/>
-                             <Route path="doctor_gourav" element={<DoctorGouravPatel/>}/>
-                             <Route path="doctor_Arun" element={<DoctorArunSharma/>}/>
-                             <Route path="doctor_ritu" element={<DoctorRituBaghel/>}/>
-                             <Route path="doctor_varsha" element={<DoctorVarshaGupta/>}/>
-
-                      </Route>
-               </Routes>
-       </BrowserRouter>
+    <h1>Wellcome To Cybrom {course}</h1>
+    <button onClick={()=>{setCourse("PHP")}}>Click Here</button>
+    <Bhopal/>
     </>
   )
 }
-export default App;
+export default App
